a5eeb0967dff38241101c68754ababae5e70dcfb
unknown <jimmyale3102@gmail.com>
2022-06-01 11:19:57 -0500

    Set UI functionality

--- app/src/main/java/dev/alejo/quoteapp/view/MainActivity.kt
+++ app/src/main/java/dev/alejo/quoteapp/view/MainActivity.kt
@@ -2,15 +2,32 @@ package dev.alejo.quoteapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import dev.alejo.quoteapp.databinding.ActivityMainBinding
+import dev.alejo.quoteapp.viewmodel.QuoteViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val quoteViewModel: QuoteViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+        initObservers()
+    }
+
+    private fun initObservers() {
+        quoteViewModel.quoteResponse.observe(this, Observer { currentQuote ->
+            binding.quote.text = currentQuote.quote
+            binding.author.text = currentQuote.author
+        })
+    }
+
+    private fun initUI() {
+        binding.quoteContainer.setOnClickListener { quoteViewModel.getRandomQuote() }
     }
 }
