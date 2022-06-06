1c6c9b04d888d3fba05bc825fe7a9776c8bfb710
unknown <jimmyale3102@gmail.com>
2022-06-06 10:06:03 -0500

    Add room module

---
+++ app/src/main/java/dev/alejo/quoteapp/di/RoomModule.kt
@@ -0,0 +1,28 @@
+package dev.alejo.quoteapp.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dev.alejo.quoteapp.data.database.QuoteDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+
+    private const val QUOTE_DATABASE_NAME = "quote_database"
+
+    @Singleton
+    @Provides
+    fun provideRoom(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, QuoteDatabase::class.java, QUOTE_DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideQuoteDao(quoteDatabase: QuoteDatabase) = quoteDatabase.quoteDao()
+
+}
